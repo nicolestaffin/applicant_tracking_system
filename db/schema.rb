862f3589a7e49d5ca9ee34b5716a5f84fb394377
Nicole Staffin <nstaffin@gmail.com>
2014-09-25 21:32:12 -0400

    fixed database congifs

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140904180233) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "applicants", force: true do |t|
     t.string  "name"
     t.integer "date_applied"
